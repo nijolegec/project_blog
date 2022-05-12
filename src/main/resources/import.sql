2c74ce21ba28f0b2494817332e46ba6cef81709e
nijolegec <nijolegec@gmail.com>
2022-05-12 11:38:49 +0300

    add i18n

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO product(title, posted_text, author_name) VALUES ('Įrašas1', 'A', 'B');
-INSERT INTO product(title, posted_text, author_name) VALUES ('Įrašas2', 'A2','B');
-INSERT INTO product(title, posted_text, author_name) VALUES ('Įrašas3', 'A3','B');
-INSERT INTO product(title, posted_text, author_name) VALUES ('Įrašas4', 'A4','B');
-INSERT INTO product(title, posted_text, author_name) VALUES ('Įrašas5', 'A5','B');
+INSERT INTO post(title, posted_text, author_name) VALUES ('Įrašas1', 'A', 'B');
+INSERT INTO post(title, posted_text, author_name) VALUES ('Įrašas2', 'A2','B');
+INSERT INTO post(title, posted_text, author_name) VALUES ('Įrašas3', 'A3','B');
+INSERT INTO post(title, posted_text, author_name) VALUES ('Įrašas4', 'A4','B');
+INSERT INTO post(title, posted_text, author_name) VALUES ('Įrašas5', 'A5','B');
